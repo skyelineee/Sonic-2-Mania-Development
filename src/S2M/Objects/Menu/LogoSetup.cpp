c1c05524f9d242fc2a8623f45564125b2a991a9f
AChickMcNuggie <79170581+AChickMcNuggie@users.noreply.github.com>
2022-08-21 02:16:45 -0400

    nugget biscuit nugget in a biscuit

---
+++ src/S2M/Objects/Menu/LogoSetup.cpp
@@ -0,0 +1,97 @@
+#include "Game.hpp"
+#include "S2M.hpp"
+#include "LogoSetup.hpp"
+
+using namespace RSDK;
+
+namespace GameLogic
+{
+RSDK_REGISTER_OBJECT(LogoSetup);
+
+void LogoSetup::Update() { state.Run(this); }
+void LogoSetup::LateUpdate() {}
+void LogoSetup::StaticUpdate() {}
+void LogoSetup::Draw() { stateDraw.Run(this); }
+
+void LogoSetup::Create(void *data)
+{
+    if (!sceneInfo->inEditor) {
+        this->active    = ACTIVE_ALWAYS;
+        this->visible   = true;
+        this->drawGroup = 12;
+        this->state.Set(SKU->region == REGION_JP ? &LogoSetup::State_CESAScreen : &LogoSetup::State_ShowLogos);
+        this->stateDraw.Set(&LogoSetup::Draw_Fade);
+        this->timer = 1024;
+    }
+}
+
+void LogoSetup::StageLoad()
+{
+    sVars->sfxSega.Get("Stage/Sega.wav");
+    GameObject::Reset(0, sVars->classID, NULL);
+    sVars->aniFrames.Load("Logos/Logos.bin", SCOPE_STAGE);
+}
+
+bool32 LogoSetup::ImageCallback()
+{
+    if (sVars->timer > 120 && (controllerInfo->keyA.press || controllerInfo->keyB.press || controllerInfo->keyStart.press || sVars->timer >= 300)) {
+        return true;
+    }
+
+    sVars->timer++;
+    return false;
+}
+
+void LogoSetup::State_CESAScreen()
+{
+    sVars->timer = 0;
+    Video::LoadImage("CESA.png", 60.0, 2.0, ImageCallback);
+
+    this->timer = 1024;
+    this->state.Set(&LogoSetup::State_ShowLogos);
+}
+
+void LogoSetup::State_ShowLogos()
+{
+    if (this->timer <= 0) {
+        if (!screenInfo->position.y)
+            sVars->sfxSega.Play();
+
+        this->timer = 0;
+        this->state.Set(&LogoSetup::State_FadeToNextLogos);
+        RSDK::StateMachine<LogoSetup> stateDraw;
+    }
+    else {
+        this->timer -= 16;
+    }
+}
+
+void LogoSetup::State_FadeToNextLogos()
+{
+    if (this->timer >= 1024) {
+        if (screenInfo->position.y >= SCREEN_YSIZE) {
+            ++sceneInfo->listPos;
+            Stage::SetScene("Presentation", "Logos");
+        }
+        else {
+            screenInfo->position.y += SCREEN_YSIZE;
+            this->state.Set(&LogoSetup::State_ShowLogos);
+            this->stateDraw.Set(&LogoSetup::Draw_Fade);
+            this->timer = 0x400;
+        }
+    }
+    else {
+        this->timer += 16;
+    }
+}
+
+void LogoSetup::Draw_Fade() { Graphics::FillScreen(0, this->timer, this->timer - 0x80, this->timer - 0x100); }
+
+#if RETRO_INCLUDE_EDITOR
+void LogoSetup::EditorDraw() {}
+
+void LogoSetup::EditorLoad() {}
+#endif
+
+void LogoSetup::Serialize() {}
+} // namespace GameLogic
---
+++ src/S2M/Objects/Menu/LogoSetup.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "Game.hpp"
+#include "S2M.hpp"
+
+namespace GameLogic
+{
+
+// Object Class
+struct LogoSetup : RSDK::GameObject::Entity {
+
+    struct Static : RSDK::GameObject::Static {
+        int32 timer;
+        RSDK::SoundFX sfxSega;
+        RSDK::SpriteAnimation aniFrames;
+    };
+
+    RSDK::StateMachine<LogoSetup> state;
+    RSDK::StateMachine<LogoSetup> stateDraw;
+    int32 timer;
+
+// ==============================
+// EVENTS
+// ==============================
+
+    void Create(void *data);
+    void Draw();
+    void Update();
+    void LateUpdate();
+    
+    static void StaticUpdate();
+    static void StageLoad();
+    static void Serialize();
+#if RETRO_INCLUDE_EDITOR
+    static void EditorDraw();
+    void EditorLoad();
+#endif
+
+// ==============================
+// FUNCTIONS
+// ==============================
+
+    static bool32 ImageCallback();
+    void Draw_Fade();
+
+    //States
+    void State_CESAScreen();
+    void State_ShowLogos();
+    void State_FadeToNextLogos();
+    void State_NextLogos();
+
+    RSDK_DECLARE(LogoSetup);
+};
+} // namespace GameLogic
